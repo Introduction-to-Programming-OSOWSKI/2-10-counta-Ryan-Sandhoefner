6fef8f6b037811cf6869962cfa19e28a4ef9c43c
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-10-06 09:41:07 -0500

    one

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function countA
+def countA(w):
+
+    a = 0
+
+    for i in range (0,len(w)):
+        if w[i] == "a":
+            a = a + 1
+    return a
+
+print (countA("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+
+
